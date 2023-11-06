cd142e32ba04afba46b46c8a195953f96fe726f2
yd2058 <yd2058@bs.amalnet.k12.il>
2023-11-06 22:47:13 +0200

    javadoc upload

--- app/src/main/java/com/example/exe6_2024/Credits.java
+++ app/src/main/java/com/example/exe6_2024/Credits.java
@@ -1,5 +1,10 @@
 package com.example.exe6_2024;
-
+/**
+ * @author		Yiftah David yd2058@bs.amalnet.k12.il
+ * @version	    1.1
+ * @since		5/11/2023
+ * this is a credits screen
+ */
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -15,11 +20,25 @@ public class Credits extends AppCompatActivity implements View.OnCreateContextMe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
     }
+    /**
+     * creates the context menu.
+     * <p>
+     *
+     * @param	menu Description	refers to the current context menu.
+     * @return	Description			returns a super action of this function.
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.main,menu);
         return super.onCreateOptionsMenu(menu);
     }
+    /**
+     * reacts to item selection.
+     * <p>
+     *
+     * @param	item Description	refers to the selected menu item.
+     * @return	Description			returns true.
+     */
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.btnm) {
@@ -27,4 +46,5 @@ public class Credits extends AppCompatActivity implements View.OnCreateContextMe
         }
         return true;
     }
+
 }
--- app/src/main/java/com/example/exe6_2024/MainActivity.java
+++ app/src/main/java/com/example/exe6_2024/MainActivity.java
@@ -1,4 +1,10 @@
 package com.example.exe6_2024;
+/**
+ * @author		Yiftah David yd2058@bs.amalnet.k12.il
+ * @version	    1.1
+ * @since		5/11/2023
+ * this program uses shared preferences in order to save a string and display it later
+ */
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,11 +29,27 @@ public class MainActivity extends AppCompatActivity implements View.OnCreateCont
         et = findViewById(R.id.et);
         strt();
     }
+    /**
+     * creates the context menu.
+     * <p>
+     *
+     * @param	menu Description	refers to the current context menu.
+     * @return	Description			returns a super action of this function.
+     */
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.main,menu);
         return super.onCreateOptionsMenu(menu);
     }
+    /**
+     * reacts to item selection.
+     * <p>
+     *
+     * @param	item Description	refers to the selected menu item.
+     * @return	Description			returns true.
+     */
+
     public boolean onOptionsItemSelected(MenuItem item){
         int id = item.getItemId();
         if(id == R.id.btnc){
@@ -37,6 +59,12 @@ public class MainActivity extends AppCompatActivity implements View.OnCreateCont
         }
         return true;
     }
+    /**
+     * saves the counter and the string to the shared preferences file.
+     * <p>
+     *
+     */
+
     public void save(){
         SharedPreferences settings = getSharedPreferences("PREFS_NAME",MODE_PRIVATE);
         SharedPreferences.Editor editor = settings.edit();
@@ -44,23 +72,48 @@ public class MainActivity extends AppCompatActivity implements View.OnCreateCont
         editor.putString("String",et.getText().toString());
         editor.commit();
     }
+    /**
+     * reacts to a counter add button press and increments the counter.
+     * <p>
+     *
+     * @param	view Description	refers to the current activity.
+     */
 
     public void add(View view) {
         count++;
         tvc.setText(count+"");
-        }
+    }
+    /**
+     * reacts to a counter reset button press and resets the counter.
+     * <p>
+     *
+     * @param	view Description	refers to the current activity.
+     */
+
     public void res(View view) {
         count = 0;
         tvc.setText(count+"");
     }
+    /**
+     * activates the save function and closes the app.
+     * <p>
+     *
+     * @param	view Description	refers to the current activity.
+     */
 
     public void out(View view) {
         save();
         finish();
     }
+    /**
+     * when the app starts the function will read the shared preferences file and fill the relevant field.
+     * <p>
+     *
+     */
     public void strt(){
         SharedPreferences settings = getSharedPreferences("PREFS_NAME",MODE_PRIVATE);
         tvc.setText(settings.getInt("Counter",0)+"");
         et.setHint(settings.getString("String","Enter Text"));
     }
+
 }
